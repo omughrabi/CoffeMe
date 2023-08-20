5021af9500ffd832df0e8f7740add2cc92fc3d8a
omughrabi <o_mugh@hotmail.com>
2023-08-20 11:10:28 +0200

    Initial Commit

---
+++ CoffeMe/CoffeMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeMeApp.swift
+//  CoffeMe
+//
+//  Created by Omar on 20.08.23.
+//
+
+import SwiftUI
+
+@main
+struct CoffeMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
